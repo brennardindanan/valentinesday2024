e6b9eee973e455807eedec2bfb45eb91a916b5f6
Brennard <brennardindanan@gmail.com>
2024-02-13 18:36:39 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = 'accept.html'; // Redirect to the Accept Page
+  });
+
+  document.getElementById('alsoYesButton').addEventListener('click', function() {
+    window.location.href = 'accept.html'; // Redirect to the Accept Page
+});
